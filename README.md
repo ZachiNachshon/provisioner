44df16b986138afdf437fb735eeb5f25f737f242
ZachiNachshon <zachi.nachshon@gmail.com>
2023-07-23 10:56:41 +0300

    Repository cleanup

--- README.md
+++
@@ -1,1 +0,0 @@
-# anchor-raspberry-pi
--- provisioner/example/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import typer
-
-from provisioner.example.dummy.cli import dummy_cli_app
-from provisioner.external.python_scripts_lib.python_scripts_lib.cli.entrypoint import main_runner
-
-app = typer.Typer(callback=main_runner)
-app.add_typer(dummy_cli_app, name="dummy")
